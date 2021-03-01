68ea30a6c23698ab3fb7922333b57f837b9bc94b
linjoshua882 <79820397+linjoshua882@users.noreply.github.com>
2021-02-28 23:19:11 -0500

    Add files via upload

---
+++ 2.28.21 Day_1/While_Loops_Practice.py
@@ -0,0 +1,11 @@
+# Initialize offset
+offset = -6
+
+# While loop
+while offset != 0 :
+    print("correcting...")
+    if offset > 0 :
+      offset = offset - 1
+    else : 
+      offset = offset + 1    
+    print(offset)
